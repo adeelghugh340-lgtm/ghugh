9d8ded9ec3980c449525782e8ece4de392e76879
unknown <you@example.com>
2026-02-20 11:17:17 +0500

    ilehfwihfwk

--- src/component/AdeelWork.jsx
+++ src/component/AdeelWork.jsx
@@ -3,20 +3,23 @@ import React from 'react'
 const AdeelWork = () => {
   return (
     <div>AdeelWork
-        <div style={{backgroundColor:"red"}}>Its testing</div>
-        <h1 style={{backgroundColor:"brown"}}>
-          <span>Maqbool</span>
-          <span>Adeel</span>
-          <span>husnain</span>
-          <span>Maqbool</span>
-          <span>Maqbool</span>
-        </h1>
-        <h1 style={{color:"red"}}>Adeel Ali</h1>
-        <input type='password' style={{border:"1px solid red",borderRadius:"2px"}}></input>
-        <h1  style={{color:"red"}}>Now i am learning git and git hub</h1>
-        <input type='color'></input>
+      <div style={{ backgroundColor: "red" }}>Its testing</div>
+      <h1 style={{ backgroundColor: "brown" }}>
+        <span>Maqbool</span>
+        <span>Adeel</span>
+        <span>husnain</span>
+        <span>Maqbool</span>
+        <span>Maqbool</span>
+      </h1>
+      <h1 style={{ color: "red" }}>Adeel Ali</h1>
+      <input type='password' style={{ border: "1px solid red", borderRadius: "2px" }}></input>
+      <h1 style={{ color: "red" }}>Now i am learning git and git hub</h1>
+      <input type='color'></input>
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores assumenda repellat exercitationem modi. Nobis quos ut perspiciatis harum reprehenderit tenetur enim excepturi, rerum doloremque, veniam temporibus iste quasi libero eius!
+      </h1>
     </div>
-    
+
   )
 }
 
