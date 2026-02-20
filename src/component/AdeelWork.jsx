3a5f6af6a5c7e52c43f5b103ba36189e7c6b07e3
unknown <you@example.com>
2026-02-20 10:53:49 +0500

    okokkpk

--- src/component/AdeelWork.jsx
+++ src/component/AdeelWork.jsx
@@ -4,7 +4,15 @@ const AdeelWork = () => {
   return (
     <div>AdeelWork
         <div style={{backgroundColor:"red"}}>Its testing</div>
+        <h1 style={{backgroundColor:"brown"}}>
+          <span>Maqbool</span>
+          <span>Adeel</span>
+          <span>husnain</span>
+          <span>Maqbool</span>
+          <span>Maqbool</span>
+        </h1>
     </div>
+    
   )
 }
 
