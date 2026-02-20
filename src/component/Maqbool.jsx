d6dbe4d3538fca6a1d95b69f1d0c00a5dd1d4c75
unknown <you@example.com>
2026-02-20 10:27:30 +0500

    jkejkfekfke

--- src/component/Maqbool.jsx
+++ src/component/Maqbool.jsx
@@ -6,6 +6,15 @@ const Maqbool = () => {
         <h1>
             Muhammad Maqbool Full Stack Engineer
         </h1>
+        <h2 >
+          <span >pakistan</span>
+          <span >punjab</span>
+          <span>pattoki</span>
+          <span>nizampura</span>
+          <input type="text">Text here</input>
+
+
+        </h2>
     </div>
   )
 }
