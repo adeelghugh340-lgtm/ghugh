f5dd45a95659c8fed9171c68492ac3bbeda430ee
adeel <adeelghugh340@gmail.com>
2026-02-20 10:50:11 +0500

    changes

--- src/component/AdeelWork.jsx
+++ src/component/AdeelWork.jsx
@@ -4,6 +4,8 @@ const AdeelWork = () => {
   return (
     <div>AdeelWork
         <div style={{backgroundColor:"red"}}>Its testing</div>
+        <h1 style={{color:"red"}}>Adeel Ali</h1>
+        <input type='password' style={{border:"1px solid red",borderRadius:"2px"}}></input>
     </div>
   )
 }
